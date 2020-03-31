ab49db335b03636889cfb9de7eea6a273524c5a9
Giyan17 <59485233+Giyan17@users.noreply.github.com>
2020-03-31 11:35:43 +0530

    Add files via upload

---
+++ Universal Bank/1.py
@@ -0,0 +1,43 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('UniversalBank.csv')
+dataset.isnull().sum() #checking if any null entry
+
+# Dividing the dataset into independet and dependent variable 
+X = dataset.iloc[:,[1,2,3,4,5,6,7,8,10,11,12,13]].values
+y = dataset.iloc[:, 9].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+####################     SVM  #################
+# Fitting Kernel SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+accuracy = (cm[0,0]+cm[1,1]) / (cm[0,0]+cm[1,1]+cm[0,1]+cm[1,0])
+print(accuracy)
+error = (cm[0,1]+cm[1,0]) / (cm[0,0]+cm[1,1]+cm[0,1]+cm[1,0])
+print(error)
+
+#####################################################
+
